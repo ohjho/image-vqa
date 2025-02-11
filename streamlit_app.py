578dc88a899cb3326bafef6ac1564ed6a00fc1d5
John Ho <jho.yvr+git@gmail.com>
2025-02-11 14:58:47 -0500

    update to use file-chat-input for multiple image prompts

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import os, sys, base64, httpx
 from io import BytesIO
 from PIL import Image
+from st_multimodal_chatinput import multimodal_chatinput
+from file_chat_input import file_chat_input
+from streamlit_float import float_init
+
+float_init()
 
 
 ### Helper functions ###
@@ -22,46 +27,11 @@ def is_valid_url(url_string):
 ### end of Helper functions ###
 
 
-def clear_st_messages():
+def clear_context():
     """clear chat history"""
+    st.session_state.pop("messages", None)
+    st.rerun()
     st.toast("chat history cleared!")
-    return st.session_state.pop("messages", None)
-
-
-def get_init_user_query():
-    assert "messages" in st.session_state, f"messages not in st.session_state"
-    assert (
-        len(st.session_state.messages) > 1
-    ), f"chat history is not long enough, only {len(st.session_state.messages)} messages found."
-    user_queries = [q for q in st.session_state.messages if q["role"] == "user"]
-    return user_queries[0]["content"]
-
-
-def get_user_image(clear_context: bool = True, force_b64: bool = True):
-    with st.sidebar.form("image_upload_form"):
-        im_url = st.text_input("Image URL")
-        uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg"])
-        if st.form_submit_button("upload image") and any([im_url, uploaded_file]):
-            if im_url and uploaded_file:
-                st.error(f"cannot provide both image file and image url")
-                return None
-            if im_url:
-                if not is_valid_url(im_url):
-                    st.warning(f"Your URL is not valid")
-                    return None
-                else:
-                    im = (
-                        image_to_base64(Image.open(BytesIO(httpx.get(im_url).content)))
-                        if force_b64
-                        else im_url
-                    )
-            elif uploaded_file:
-                im = Image.open(uploaded_file)
-                im = image_to_base64(im)
-            if clear_context:
-                clear_st_messages()
-            st.session_state["image"] = im
-    return st.session_state["image"] if "image" in st.session_state else None
 
 
 def get_openrouter_api_key():
@@ -108,22 +78,9 @@ def build_llm(
     )
 
 
-def generate_response(llm, query, im, use_context: bool = True):
+def generate_response(llm):
     """Function for generating LLM response"""
-    add_context = len(st.session_state.messages) > 1 and use_context
-    init_query = get_init_user_query() if add_context else query
-
-    message = [
-        {
-            "role": "user",
-            "content": [
-                {"type": "text", "text": init_query},
-                {"type": "image_url", "image_url": im},
-            ],
-        }
-    ]
-    if add_context:
-        message += st.session_state.messages[2:]
+    message = st.session_state.messages
     msg = st.toast(f"message context: {len(message)}")
     try:
         r = llm.invoke(message)
@@ -133,9 +90,66 @@ def generate_response(llm, query, im, use_context: bool = True):
         return None
 
 
+def chatinput2msg(chatinput):
+    if not chatinput:
+        return None
+    if not chatinput["message"]:  # ["text"]:
+        st.toast("Your input must contain text")
+        return None
+
+    im_msg = []
+    if chatinput["files"]:  # ["images"]:
+        for im in chatinput["files"]:  # ["images"]:
+            if im["content"].startswith("data:image"):
+                im_msg.append({"type": "image_url", "image_url": im["content"]})
+            else:
+                st.toast(f"Problem loading {im} from your input")
+                return None
+
+    if im_msg:
+        return {
+            "role": "user",
+            "content": [
+                # {"type": "text", "text": chatinput["text"]},
+                {"type": "text", "text": chatinput["message"]},
+            ]
+            + im_msg,
+        }
+    else:
+        # return {"role": "user", "content": chatinput["text"]}
+        return {"role": "user", "content": chatinput["message"]}
+
+
+def get_mminput(st_container):
+    msg = None
+    # chatinput = multimodal_chatinput(
+    #     default=None,
+    #     disabled=False,  # placeholder="Ask me anything about images..."
+    # )
+    with st_container:
+        chatinput = file_chat_input("Ask me anything about an image...")
+    if chatinput:
+        msg = chatinput2msg(chatinput)
+    return msg
+
+
+def write_message(msg, st_container):
+    with st_container:
+        if type(msg["content"]) == str:
+            st.write(msg["content"])
+        elif type(msg["content"]) == list:
+            for m in msg["content"]:
+                if m["type"] == "text":
+                    st.write(m["text"])
+                elif m["type"] == "image_url":
+                    st.image(m["image_url"])
+        else:
+            return None
+
+
 def Main():
     # App title
-    st.set_page_config(page_title="🤗💬 Image Q&A")
+    # st.set_page_config(page_title="🤗💬 Image Q&A")
     st.sidebar.title("🤗💬 Image Q&A")
     api_key = get_openrouter_api_key()
 
@@ -156,13 +170,8 @@ def Main():
         llm = build_llm(api_key, model_name=model_name, temperature=temperature)
         msg = st.toast(f"LLM {model_name} loaded from OpenRouter")
 
-    # Get User's Image
-    im = get_user_image()
-    if not im:
-        st.warning(f":point_left: Provide your image to get started")
-        return None
-    else:
-        st.sidebar.image(im, caption="uploaded image")
+    if st.sidebar.button(f"Clear Context"):
+        clear_context()
 
     # Store LLM generated responses
     if "messages" not in st.session_state.keys():
@@ -175,20 +184,27 @@ def Main():
 
     # Display chat messages
     for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.write(message["content"])
+        write_message(message, st_container=st.chat_message(message["role"]))
+        # with st.chat_message(message["role"]):
+        #     st.write(message["content"])
+
+    # Chat Layout management
+    st_msg_board = st.empty()
+    st_thinking_placeholder = st.empty()
+    msg_container = st.container()
+    msg_container.float("bottom: 0")
 
     # User-provided prompt
-    if prompt := st.chat_input(disabled=not (api_key and im)):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.write(prompt)
+    user_input = get_mminput(msg_container)
+    if user_input:
+        st.session_state.messages.append(user_input)
+        write_message(user_input, st_container=st_msg_board.chat_message("user"))
 
     # Generate a new response if last message is not from assistant
     if st.session_state.messages[-1]["role"] != "assistant":
-        with st.chat_message("assistant"):
+        with st_thinking_placeholder.chat_message("assistant"):
             with st.spinner("Thinking..."):
-                response = generate_response(llm, query=prompt, im=im)
+                response = generate_response(llm)  # , query=prompt, im=im)
                 st.write(response)
         message = {"role": "assistant", "content": response}
         st.session_state.messages.append(message)
